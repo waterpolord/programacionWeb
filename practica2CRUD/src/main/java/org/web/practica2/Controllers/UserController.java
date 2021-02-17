a436dd4291bd2ea3b918852bfa90df42d5101d9b
Robert Garcia <robertgarcia341@gmail.com>
2021-02-17 19:31:09 -0400

    agregando servicios y el controlador del usuario

---
+++ practica2CRUD/src/main/java/org/web/practica2/Controllers/UserController.java
@@ -0,0 +1,11 @@
+package org.web.practica2.Controllers;
+
+import io.javalin.Javalin;
+
+public class UserController {
+    private Javalin app;
+    public UserController(Javalin app ){
+        this.app = app;
+    }
+
+}
---
+++ practica2CRUD/src/main/java/org/web/practica2/Services/Principal.java
@@ -0,0 +1,75 @@
+package org.web.practica2.Services;
+
+import org.web.practica2.models.Client;
+import org.web.practica2.models.Product;
+import org.web.practica2.models.Sell;
+import org.web.practica2.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+    private static Principal principal;
+    private ArrayList<User> users =  new ArrayList<>();
+    private ArrayList<Client> clients =  new ArrayList<>();
+    private ArrayList<Product> products =  new ArrayList<>();
+    private ArrayList<Sell> sells =  new ArrayList<>();
+
+    public Principal(){
+        users.add(new User("Robert","Admin","admin"));
+
+    }
+
+    public static Principal getInstance(){
+        if(principal == null){
+            principal = new Principal();
+        }
+        return principal;
+    }
+
+    // metodos del usuario
+    public Boolean userExistByUsername(String username){
+        for (User user:users){
+            if(user.getUsername().equalsIgnoreCase(username)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public User findUserByUsername(String username){
+        for (User user:users){
+            if(user.getUsername().equalsIgnoreCase(username)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void addUser(User user){
+        if(!userExistByUsername(user.getUsername())){
+            users.add(user);
+        }
+    }
+
+
+
+    // getters
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public ArrayList<Sell> getSells() {
+        return sells;
+    }
+
+}
